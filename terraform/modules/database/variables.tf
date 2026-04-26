252d838463164f9d647be4cced209e750587f7e1
mtarek210048-dev <mtarek210048@gmail.com>
2026-04-26 12:05:37 +0300

    Update variables.tf

--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -1,10 +1,10 @@
-variable "project_name"       { type = string }
-variable "environment"        { type = string }
-variable "vpc_id"             { type = string }
+variable "project_name"  { type = string }
+variable "environment" { type = string }
+variable "vpc_id" { type = string }
 variable "private_subnet_ids" { type = list(string) }
-variable "app_sg_id"          { type = string }
-variable "db_name"            { type = string }
-variable "db_username"        { type = string }
+variable "app_sg_id" { type = string }
+variable "db_name" { type = string }
+variable "db_username"  { type = string }
 variable "db_password" {
   type      = string
   sensitive = true
